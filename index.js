0e40844b9e618b638a194d57895a80ac0b02c56f
SalamanderCtesiphon <swbrookshire@gmail.com>
2025-02-24 15:45:43 -0600

    completed the assignment. it was fun

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ let root = 0;
 
 setRes.addEventListener("click", () => {
   root = prompt("Enter Number of Pixels per side:");
+  if(root < 2 || root >= 100) {
+    const container = document.querySelector("#container");
+    container.textContent = "Try again Sucker! Pick a number greater than two but less than 100!";
+    return;
+  }
   makeDivs(root);
 });
 
 function makeDivs () {
 
   const container = document.querySelector("#container");
+  container.textContent = "";
   let num = root * root;
 
   let rowNum = "";
@@ -22,7 +28,6 @@ function makeDivs () {
   container.style.gridTemplateRows = rowNum;
   for(let i = 1; i <= num; i++) {
     const div = document.createElement("div");
-    div.style.border = "1px solid black";
     div.style.flex = 1;
     div.setAttribute("class", "divChild");
     div.addEventListener("mouseover", () => {
