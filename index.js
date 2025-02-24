5233bf9d7534c8a8a8833a30f4632e25cbed1f8f
SalamanderCtesiphon <swbrookshire@gmail.com>
2025-02-24 15:02:42 -0600

    accomplished the basic set up per instructions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello");
+const container = document.querySelector("#container");
+
+
+function makeDivs (container) {
+  let root = 16;
+  let num = root * root;
+
+  let rowNum = "";
+
+  for(let i = 1; i <= root; i++) {
+    rowNum += "auto "
+
+  }
+  console.log(rowNum);
+  container.style.gridTemplateColumns = rowNum;
+  container.style.gridTemplateRows = rowNum;
+  for(let i = 1; i <= num; i++) {
+    const div = document.createElement("div");
+    div.style.border = "1px solid black";
+    div.style.flex = 1;
+    div.setAttribute("class", "divChild");
+    container.appendChild(div);
+
+    }
+};
+
+makeDivs(container);
